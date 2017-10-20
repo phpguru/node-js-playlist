2250b25361000791b19d7b4dcd22c4be3d187a58
phpguru <geoffrey.hoffman@gmail.com>
2017-10-19 20:12:16 -0700

    Lesson 34 ish

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,12 +1,23 @@
 // ToDo Controller
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+var data = [
+	{item: 'Get Milk'},
+	{item: 'Walk dog'},
+	{item: 'Kick some coding ass'}
+];
+
 module.exports = function(app){
 
 	app.get('/todo', function(req, res){
-		res.render('todo')
+		res.render('todo', {todos: data});
 	});
 
-	app.post('/todo', function(req, res){
-		
+	app.post('/todo', urlencodedParser, function(req, res){
+		data.push(req.body);
+		//res.render('todo', {todos: data});
+		res.json({todos: data});
 	});
 
 	app.delete('/todo', function(req, res){
