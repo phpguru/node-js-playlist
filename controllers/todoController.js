f8df39b4aea3f8f9a843b351bede6bd7be76049a
phpguru <geoffrey.hoffman@gmail.com>
2017-10-28 09:49:03 -0700

    Added mongodb and finished Net Ninja Node Tutorial

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,26 +1,53 @@
 // ToDo Controller
 var bodyParser = require('body-parser');
+
+var fs = require('fs');
+var connection = fs.readFileSync('connection.txt', 'utf8');
+console.log(connection);
+
+
+var mongoose = require('mongoose');
+// Connect to database
+mongoose.connect(connection);
+// Schema for database
+var todoSchema = new mongoose.Schema({
+	item: String
+});
+// Model
+var Todo = mongoose.model('Todo', todoSchema);
+
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-var data = [
-	{item: 'Get Milk'},
-	{item: 'Walk dog'},
-	{item: 'Kick some coding ass'}
-];
+// var data = [
+// 	{item: 'Get Milk'},
+// 	{item: 'Walk dog'},
+// 	{item: 'Kick some coding ass'}
+// ];
 
 module.exports = function(app){
 
 	app.get('/todo', function(req, res){
-		res.render('todo', {todos: data});
+		// get data from mongodb
+		Todo.find({}, function(err, data){
+			if (err) throw err;
+			res.render('todo', {todos: data});
+		})
 	});
 
 	app.post('/todo', urlencodedParser, function(req, res){
-		data.push(req.body);
-		//res.render('todo', {todos: data});
-		res.json({todos: data});
+		// get data from the view and add to mongodb
+		var newTodo = Todo(req.body).save(function(err, data){
+			if (err) throw err;
+			res.json({todos: data});
+		});
 	});
 
-	app.delete('/todo', function(req, res){
+	app.delete('/todo/:item', function(req, res){
+		// delete the requested item from mongodb
+		Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+			if (err) throw err;
+			res.json(data);
+		});
 		
 	});
 
